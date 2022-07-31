577295f3563f5008e677b016b313d28b63ae7b9a
yanshouwang <yanshouwang@outlook.com>
2022-07-31 10:14:30 +0800

    优化导入顺序

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as util from "./util";
 import * as path from "path";
+import * as util from "./util";
 import { SnippetCompletionItemProvider } from './providers';
 
 // this method is called when your extension is activated
--- src/extension/util.ts
+++ src/extension/util.ts
@@ -1,6 +1,6 @@
-import * as fs from "fs";
-import * as path from "path";
 import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 
 export const isWin = process.platform.startsWith("win");
 
